df7a180e8472f6efc883a801ced3ffa97402f13e
Nick S <nickjsaulsberry@outlook.com>
2020-04-06 18:08:05 -0500

    created greeting page route

---
+++ lib/Screens/Greeting/Greeting.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class Greeting extends StatefulWidget {
+  @override
+  _GreetingState createState() => _GreetingState();
+}
+
+class _GreetingState extends State<Greeting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
