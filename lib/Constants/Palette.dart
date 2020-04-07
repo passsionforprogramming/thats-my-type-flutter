0b7cfcc2371cd0a5b451d422d16f705e9b1bda3a
Nick S <nickjsaulsberry@outlook.com>
2020-04-07 17:30:32 -0500

    Added color palette

---
+++ lib/Constants/Palette.dart
@@ -0,0 +1,11 @@
+import 'dart:developer';
+
+import "package:flutter/material.dart";
+
+const kBackgroundColor = Color(0xFFf3dc4); //That's my type background color
+const kAccentBlack = Color(0xFF000000); //black color
+const kAccentDarkerColor = Color(0xFFf2a68d); //accent color
+
+//Second Set
+const kDarkerAccentRedish = Color(0xFFdd7564);
+const kOtherBackgroundColor = Color(0xFFf0ede6);
