d549d47a850a58e0b6c25b371bb942c6036a3050
Nick S <nickjsaulsberry@outlook.com>
2020-04-07 19:58:59 -0500

    Added animated intro text

--- lib/Constants/Palette.dart
+++ lib/Constants/Palette.dart
@@ -2,10 +2,14 @@ import 'dart:developer';
 
 import "package:flutter/material.dart";
 
-const kBackgroundColor = Color(0xFFf3dc4); //That's my type background color
+const kBackgroundColor = Color(0xFFf3dcd4); //That's my type background color
 const kAccentBlack = Color(0xFF000000); //black color
 const kAccentDarkerColor = Color(0xFFf2a68d); //accent color
+const kDarkerBackgroundColor = Color(0xFFe0cbc4);
 
 //Second Set
 const kDarkerAccentRedish = Color(0xFFdd7564);
 const kOtherBackgroundColor = Color(0xFFf0ede6);
+const kGreen = Color(0xFF20c1bd);
+const kDarkerGreen = Color(0xFF1fa5a6);
+const klighterGreen = Color(0xFFb2dedd);
--- lib/Screens/Greeting/Greeting.dart
+++ lib/Screens/Greeting/Greeting.dart
@@ -1,13 +1,103 @@
+import 'dart:async';
 import "package:flutter/material.dart";
+import 'package:thatismytype/Constants/Palette.dart';
 
 class Greeting extends StatefulWidget {
   @override
   _GreetingState createState() => _GreetingState();
 }
 
-class _GreetingState extends State<Greeting> {
+class _GreetingState extends State<Greeting> with TickerProviderStateMixin {
+  AnimationController firstAnimationController;
+  AnimationController secondAnimationController;
+  AnimationController thirdAnimationController;
+  Animation firstWordAnimation;
+  Animation secondWordAnimation;
+  Animation thirdWordAnimation;
+  @override
+  void initState() {
+    firstAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    secondAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    thirdAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    firstWordAnimation = CurvedAnimation(
+        parent: firstAnimationController, curve: Curves.decelerate)
+      ..addListener(() {
+        setState(() {
+          print(firstWordAnimation.value);
+        });
+      });
+    secondWordAnimation = CurvedAnimation(
+        parent: secondAnimationController, curve: Curves.decelerate)
+      ..addListener(() {
+        setState(() {});
+      });
+    thirdWordAnimation = CurvedAnimation(
+        parent: thirdAnimationController, curve: Curves.decelerate)
+      ..addListener(() {
+        setState(() {});
+      });
+    staggerFade();
+    super.initState();
+  }
+
+  void trackState() {
+    setState(() {});
+  }
+
+  staggerFade() {
+    Timer(Duration(seconds: 1), () => firstAnimationController.forward());
+    Timer(Duration(seconds: 2), () => secondAnimationController.forward());
+    Timer(Duration(seconds: 4), () => thirdAnimationController.forward());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: kDarkerBackgroundColor,
+      body: Column(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(top: screenHeight * .1),
+              child: Container(
+                margin: EdgeInsets.only(left: screenWidth * .01),
+                child: Text(
+                  "That's",
+                  style: TextStyle(
+                      fontSize: 36.0,
+                      fontFamily: "Playfair",
+                      color:
+                          kAccentBlack.withOpacity(firstWordAnimation.value)),
+                ),
+              )),
+          Container(
+              margin: EdgeInsets.only(top: screenHeight * .02),
+              child: Container(
+                margin: EdgeInsets.only(left: screenWidth * .30),
+                child: Text("My",
+                    style: TextStyle(
+                        fontSize: 36.0,
+                        fontFamily: "Playfair",
+                        color: kAccentBlack
+                            .withOpacity(secondWordAnimation.value))),
+              )),
+          Container(
+              margin: EdgeInsets.only(top: screenHeight * .02),
+              child: Container(
+                margin: EdgeInsets.only(left: screenWidth * .5),
+                child: Text("Type",
+                    style: TextStyle(
+                        fontSize: 36.0,
+                        fontFamily: "Playfair",
+                        color: kAccentBlack
+                            .withOpacity(thirdWordAnimation.value))),
+              ))
+        ],
+      ),
+    );
   }
 }
