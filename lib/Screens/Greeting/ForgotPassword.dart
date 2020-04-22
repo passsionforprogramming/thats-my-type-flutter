7e238e129c4b688c517fdee53904d4ef2f2db84f
Nick S <nickjsaulsberry@outlook.com>
2020-04-22 18:21:04 -0500

    completed reset password and styling

--- lib/Screens/Greeting/ForgotPassword.dart
+++ lib/Screens/Greeting/ForgotPassword.dart
@@ -41,15 +41,17 @@ class _ForgotPasswordState extends State<ForgotPassword> {
         setState(() {
           displayWarningBox = true;
         });
-      } catch (e) {}
+      } catch (e) {
+        print(e);
+      }
     }
   }
 
   warningBox(screenWidth, screenHeight) {
     return Container(
         color: kDarkerAccentRedish,
-        padding: EdgeInsets.all(10.0),
-        margin: EdgeInsets.symmetric(vertical: screenHeight * .05),
+        padding: EdgeInsets.all(screenWidth * .03),
+        margin: EdgeInsets.only(bottom: screenHeight * .03),
         child: Row(
           children: <Widget>[
             Icon(
@@ -58,22 +60,34 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               size: 35.0,
             ),
             SizedBox(
-              width: screenWidth * .1,
+              width: screenWidth * .04,
             ),
-            Text(
-              "A password reset links has been sent to $email",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "Gothic Semi-bold",
-                  fontSize: 18.0),
+            Container(
+              width: screenWidth * .6,
+              child: Text(
+                "A password reset link has been sent to $email",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Gothic Semi-bold",
+                    fontSize: 14.0),
+              ),
             ),
             SizedBox(
-              width: screenWidth * .1,
+              width: screenWidth * .04,
             ),
-            Icon(
-              Icons.close,
-              color: Colors.white,
-              size: 35.0,
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    PageTransition(
+                        child: Greeting(),
+                        type: PageTransitionType.leftToRight));
+              },
+              child: Icon(
+                Icons.close,
+                color: Colors.white,
+                size: 35.0,
+              ),
             )
           ],
         ));
@@ -85,95 +99,99 @@ class _ForgotPasswordState extends State<ForgotPassword> {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: kDarkerBackgroundColor,
-      body: ListView(
-        children: <Widget>[
-          displayWarningBox
-              ? warningBox(screenWidth, screenHeight)
-              : SizedBox(),
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(
-                vertical: screenHeight * .05, horizontal: screenWidth * .1),
-            width: screenWidth * .8,
-            child: Text(
-              "Reset Password",
-              style: TextStyle(
-                  color: kDarkerAccentRedish,
-                  fontFamily: "Playfair Bold",
-                  fontSize: 36.0),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: screenWidth * .1),
-            color: Colors.white10,
-            width: screenWidth * .8,
-            child: TextFormField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.done,
-              onFieldSubmitted: (val) {
-                sendPasswordLink();
-              },
-              validator: emailValidator,
-              decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Enter email here...",
-                  labelStyle: TextStyle(color: kDarkBlue),
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: kDarkBlue,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: kDarkBlue,
-                          width: 1.0,
-                          style: BorderStyle.solid)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: kDarkBlue,
-                          width: 3.0,
-                          style: BorderStyle.solid))),
-            ),
-          ),
-          Container(
-            width: screenWidth * .9,
-            margin: EdgeInsets.symmetric(
-                vertical: screenHeight * .05, horizontal: screenWidth * .05),
-            child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0)),
-                padding: EdgeInsets.symmetric(vertical: screenHeight * .02),
-                color: kDarkerAccentRedish,
-                onPressed: sendPasswordLink,
-                child: Text(
-                  "Get Reset Link",
-                  style: TextStyle(
-                      fontFamily: "Gothic Semi-bold",
-                      color: Colors.white,
-                      fontSize: 18.0),
-                )),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  PageTransition(
-                      child: Greeting(), type: PageTransitionType.leftToRight));
-            },
-            child: Container(
-              margin: EdgeInsets.only(top: screenHeight * .01),
+      body: Form(
+        key: resetPasswordKey,
+        child: ListView(
+          children: <Widget>[
+            displayWarningBox
+                ? warningBox(screenWidth, screenHeight)
+                : SizedBox(),
+            Container(
               alignment: Alignment.center,
-              width: .8 * screenWidth,
+              margin: EdgeInsets.symmetric(
+                  vertical: screenHeight * .05, horizontal: screenWidth * .1),
+              width: screenWidth * .8,
               child: Text(
-                "Return to Sign In",
+                "Reset Password",
                 style: TextStyle(
-                    color: kDarkBlue,
-                    fontSize: 17.0,
-                    fontFamily: "Josefin Sans Bold"),
+                    color: kDarkerAccentRedish,
+                    fontFamily: "Playfair Bold",
+                    fontSize: 36.0),
               ),
             ),
-          )
-        ],
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: screenWidth * .1),
+              color: Colors.white10,
+              width: screenWidth * .8,
+              child: TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (val) {
+                  sendPasswordLink();
+                },
+                validator: emailValidator,
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter email here...",
+                    labelStyle: TextStyle(color: kDarkBlue),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: kDarkBlue,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: kDarkBlue,
+                            width: 1.0,
+                            style: BorderStyle.solid)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: kDarkBlue,
+                            width: 3.0,
+                            style: BorderStyle.solid))),
+              ),
+            ),
+            Container(
+              width: screenWidth * .9,
+              margin: EdgeInsets.symmetric(
+                  vertical: screenHeight * .05, horizontal: screenWidth * .05),
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0)),
+                  padding: EdgeInsets.symmetric(vertical: screenHeight * .02),
+                  color: kDarkerAccentRedish,
+                  onPressed: sendPasswordLink,
+                  child: Text(
+                    "Get Reset Link",
+                    style: TextStyle(
+                        fontFamily: "Gothic Semi-bold",
+                        color: Colors.white,
+                        fontSize: 18.0),
+                  )),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    PageTransition(
+                        child: Greeting(),
+                        type: PageTransitionType.leftToRight));
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: screenHeight * .01),
+                alignment: Alignment.center,
+                width: .8 * screenWidth,
+                child: Text(
+                  "Return to Sign In",
+                  style: TextStyle(
+                      color: kDarkBlue,
+                      fontSize: 17.0,
+                      fontFamily: "Josefin Sans Bold"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
