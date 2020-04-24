f560daed5da27ee4755be14772156edef7cba038
Nick S <nickjsaulsberry@outlook.com>
2020-04-23 19:01:58 -0500

    added date picker and formatting with submit button and name

---
+++ lib/Screens/ProfileSetup/HelloScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HelloScreen extends StatefulWidget {
+  @override
+  _HelloScreenState createState() => _HelloScreenState();
+}
+
+class _HelloScreenState extends State<HelloScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/Screens/ProfileSetup/IntroduceYourself.dart
+++ lib/Screens/ProfileSetup/IntroduceYourself.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
+import 'package:intl/intl.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:thatismytype/Constants/Palette.dart';
+import 'package:flutter_time_date_picker/flutter_time_date_picker.dart';
+import 'package:thatismytype/Screens/ProfileSetup/HelloScreen.dart';
 
 class IntroduceYourself extends StatefulWidget {
   @override
@@ -8,6 +14,49 @@ class IntroduceYourself extends StatefulWidget {
 
 class _IntroduceYourselfState extends State<IntroduceYourself> {
   String gender;
+  DateTime birthday;
+  List<String> errors = [];
+
+  showDatePicker() {
+    DatePicker.showDatePicker(context,
+        initialDateTime: DateTime.now().subtract(Duration(days: 365 * 21)),
+        locale: DATETIME_PICKER_LOCALE_DEFAULT,
+        pickerMode: DateTimePickerMode.date, onChange: (date, list) {
+      setState(() {
+        print("Here is the date $date");
+        birthday = date;
+      });
+    });
+  }
+
+  displayDatePicker(screenHeight, screenWidth) {
+    return showAnimatedDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)),
+            elevation: 5.0,
+            content: Container(
+              height: screenHeight * .5,
+              width: screenWidth * 6,
+              child: CupertinoDatePicker(
+                  mode: CupertinoDatePickerMode.date,
+                  initialDateTime:
+                      DateTime.now().subtract(Duration(days: 365 * 21)),
+                  onDateTimeChanged: (date) {
+                    setState(() {
+                      birthday = date;
+                    });
+                  }),
+            ),
+          );
+        },
+        animationType: DialogTransitionType.scale,
+        barrierDismissible: true,
+        duration: Duration(milliseconds: 200),
+        curve: Curves.bounceIn);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -133,10 +182,72 @@ class _IntroduceYourselfState extends State<IntroduceYourself> {
             decoration: BoxDecoration(
                 border: Border(
                     bottom: BorderSide(width: 1.0, color: Colors.black26))),
-            child: Text(
-              "MM/DD/YYYY",
-              style: TextStyle(
-                  color: Colors.black54, fontSize: 18.0, fontFamily: "Raleway"),
+            child: GestureDetector(
+              onTap: () => showDatePicker(),
+              child: Text(
+                birthday == null
+                    ? "MM/DD/YYYY"
+                    : DateFormat.yMd("en_US").format(birthday),
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18.0,
+                    fontFamily: "Raleway"),
+              ),
+            ),
+          ),
+          Container(
+            width: screenWidth * .8,
+            margin: EdgeInsets.symmetric(horizontal: screenWidth * .1),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              validator: (val) {
+                if (val.length > 2) return null;
+                return "The name must be at least two characters.";
+              },
+              onFieldSubmitted: (val) {
+                Navigator.pushReplacement(
+                    context,
+                    PageTransition(
+                        child: HelloScreen(), type: PageTransitionType.scale));
+              },
+              decoration: InputDecoration(
+                  labelText: "Name",
+                  labelStyle: TextStyle(fontFamily: "Raleway"),
+                  hintText: "Add your first name"),
+            ),
+          ),
+          errors.length > 0
+              ? Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: screenWidth * .1),
+                  child: Column(
+                      children: errors
+                          .map((error) => Text(
+                                error,
+                                style: TextStyle(color: Colors.red),
+                              ))
+                          .toList()))
+              : SizedBox(),
+          Container(
+            margin: EdgeInsets.symmetric(
+                horizontal: screenWidth * .1, vertical: screenHeight * .15),
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    PageTransition(
+                        child: HelloScreen(),
+                        type: PageTransitionType.scale,
+                        duration: Duration(milliseconds: 200)));
+              },
+              color: kDarkerGreen,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0)),
+              padding: EdgeInsets.symmetric(vertical: 14.0),
+              child: Text(
+                "Continue",
+                style: TextStyle(color: Colors.white, fontSize: 17.0),
+              ),
             ),
           )
         ],
